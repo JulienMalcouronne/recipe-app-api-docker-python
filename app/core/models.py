7c515b1589ccb1ad5d6eddd8769164144902a377
Julien Malcouronne <malcouronnejulien@gmail.com>
2022-01-06 07:54:09 +0100

    some quick syntax fix

--- app/core/models.py
+++ app/core/models.py
@@ -15,13 +15,12 @@ class UserManager(BaseUserManager):
 
         return user
 
-
     def create_superuser(self, email, password):
         """Creates and saves a new super user"""
         user = self.create_user(email, password)
-        user.is_staff= True
+        user.is_staff = True
         user.is_superuser = True
-        user.save(using=self.db)
+        user.save(using=self._db)
 
         return user
 
